4bfb153f4643a9761cb59ca44f67f4d972cadd42
Joey HU <joeyhu@Joeys-MacBook-Pro.local>
2021-12-31 00:59:05 +1100

    connect api

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import millify from 'millify';
+import {Typography, Row, Col, Statistic} from 'antd';
+import {Link} from 'react-router-dom';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const {Title} =Typography;
 
 const Homepage = () => {
+    const{ data, isFetching} = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+
+    if (isFetching) return 'Loading...';
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+            <Title level={2} className="heading"> Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title="Totle Cryptocurrencies" value={globalStats.total} /></Col>
+                <Col span={12}><Statistic title="Totle Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Totle Market Cap" value= {millify(globalStats.totalMarketCap)}/></Col>
+                <Col span={12}><Statistic title="Totle 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
+                <Col span={12}><Statistic title="Totle Markets" value={millify(globalStats.totalMarkets)} /></Col>
+            </Row>
+        </>
     )
 }
 
